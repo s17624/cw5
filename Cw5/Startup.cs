f56a99a8bf8a937c7989edbe2aba0a76a86f61e6
s17624 <s17624@pjwstk.edu.pl>
2020-05-18 00:13:54 +0200

    startup and controller fix
    Signed-off-by: s17624 <s17624@pjwstk.edu.pl>

--- Cw5/Startup.cs
+++ Cw5/Startup.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Cw5
 {
@@ -24,16 +26,54 @@ namespace Cw5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<Services.IDbService, Services.SqlSerrverDbService>();
             services.AddControllers();
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Students App API",
+                    Version = "v1.0"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Services.IDbService service)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMiddleware<Middleware.ExceptionMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/vs/swagger.json", "Students App API");
+            });
+
+            app.UseMiddleware<Middleware.LoggingMiddleware>();
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Podaj numer indeksu");
+                    return;
+                }
+
+                String index = context.Request.Headers["Index"].ToString();
+                var student = service.getStudent(index);
+                if (student == null)
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    await context.Response.WriteAsync("Nie ma studenta z takim indexem");
+                    return;
+                }
+                await next();
+            });
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
